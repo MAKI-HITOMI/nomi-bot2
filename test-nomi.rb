4e0922d0cc602a79de819bddcc383364ed01849b
MAKI <47407356+MAKI-HITOMI@users.noreply.github.com>
2019-07-22 17:08:20 +0900

    Update test-nomi.rb

--- test-nomi.rb
+++ test-nomi.rb
@@ -1,10 +1,10 @@
 ﻿require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "MY_CONSUMER_KEY"
-  config.consumer_secret     = "MY_CONSUMER_SECRET"
-  config.access_token        = "MY_ACCESS_TOKEN"
-  config.access_token_secret = "MY_ACCESS_TOKEN_SECRET"
+  config.consumer_key        = ENV['MY_CONSUMER_KEY']        #Consumer Key (API Key)
+  config.consumer_secret     = ENV['MY_CONSUMER_SECRET']     #Consumer Secret (API Secret)
+  config.access_token        = ENV['MY_ACCESS_TOKEN']        #Access Token
+  config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET'] #Access Token Secret
 end
 
 client.update("ぴちぴちビキニのハマチと愉快なココナッツ投げ祭り")
