ddf209ea97dbb06f1e8f77bf9c54fd519e9487d0
MAKI <47407356+MAKI-HITOMI@users.noreply.github.com>
2019-07-23 03:56:11 +0900

    Update test-nomi.rb

--- test-nomi.rb
+++ test-nomi.rb
@@ -1,10 +1,10 @@
 require 'twitter'
 
 @client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = 
-  config.access_token_secret = 
+  config.consumer_key        = ENV['MY_CONSUMER_KEY']
+  config.consumer_secret     = ENV['MY_CONSUMER_SECRET']
+  config.access_token        = ENV['MY_ACCESS_TOKEN']
+  config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
 now1 =DateTime.now
