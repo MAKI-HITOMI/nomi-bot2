219603ca9b5967ba1e8fd881f44e44b63aa60963
MAKI <47407356+MAKI-HITOMI@users.noreply.github.com>
2019-07-22 16:33:53 +0900

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+bot: ruby test-nomi.rb
---
+++ test-nomi.rb
@@ -0,0 +1,11 @@
+﻿require 'twitter'
+ENV['SSL_CERT_FILE'] = File.expand_path('./cacert.pem')
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "MY_CONSUMER_KEY"
+  config.consumer_secret     = "MY_CONSUMER_SECRET"
+  config.access_token        = "MY_ACCESS_TOKEN"
+  config.access_token_secret = "MY_ACCESS_TOKEN_SECRET"
+end
+
+client.update("ぴちぴちビキニのハマチと愉快なココナッツ投げ祭り")
